c909b9ddbe90742c2010c3573e12df89c5bb14d4
chirag <chiragpgauswami@gmail.com>
2026-03-02 12:24:18 +0530

    Changes in Send Gift

--- controllers/customer.controller.js
+++ controllers/customer.controller.js
@@ -1922,7 +1922,7 @@ const giftRandomShopItemInRoom = async (req, res) => {
           // Hit Socket event in room
           ////////////////////////////////////////////////////////
           io.to(userId).emit("treasureBoxItem", {
-            message: `You have received ${randomItem.xp} XP as a gift!`,
+            message: `You have received ${randomItem.xp} EXP as a gift!`,
             image:
               "https://usefun-uploads.s3.ap-south-1.amazonaws.com/1000089358-removebg-preview.png",
           });
