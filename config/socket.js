55aa16f2f151f94c457210de9cfdcdce8a0deab8
chirag <chiragpgauswami@gmail.com>
2026-03-03 12:25:02 +0530

    Changes in Send Gift

--- config/socket.js
+++ config/socket.js
@@ -523,7 +523,7 @@ const configure = async (app, server) => {
 
               // Hit Socket event in room
               ////////////////////////////////////////////////////////
-              io.to(userId).emit("treasureBoxItem", {
+              io.to(userId.toString()).emit("treasureBoxItem", {
                 item: finalItemdata,
                 message: `You have received a ${finalItemdata.name} as a gift!`,
               });
@@ -538,7 +538,7 @@ const configure = async (app, server) => {
 
               // Hit Socket event in room
               ////////////////////////////////////////////////////////
-              io.to(userId).emit("treasureBoxItem", {
+              io.to(userId.toString()).emit("treasureBoxItem", {
                 message: `You have received ${randomItem.diamondAmount} diamonds as a gift!`,
                 image:
                   "https://usefun-uploads.s3.ap-south-1.amazonaws.com/1000089129-removebg-preview.png",
@@ -554,7 +554,7 @@ const configure = async (app, server) => {
 
               // Hit Socket event in room
               ////////////////////////////////////////////////////////
-              io.to(userId).emit("treasureBoxItem", {
+              io.to(userId.toString()).emit("treasureBoxItem", {
                 message: `You have received ${randomItem.beansAmount} beans as a gift!`,
                 image:
                   "https://usefun-uploads.s3.ap-south-1.amazonaws.com/beans.png",
@@ -574,7 +574,7 @@ const configure = async (app, server) => {
 
               // Hit Socket event in room
               ////////////////////////////////////////////////////////
-              io.to(userId).emit("treasureBoxItem", {
+              io.to(userId.toString()).emit("treasureBoxItem", {
                 message: `You have received ${randomItem.xp} EXP as a gift!`,
                 image:
                   "https://usefun-uploads.s3.ap-south-1.amazonaws.com/1000089358-removebg-preview.png",
